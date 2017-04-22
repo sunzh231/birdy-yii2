3431c97ec99c63c9c26f204624f8766c29f6bd47
孙宗衡 <sunzh@huntor.cn>
2017-04-22 09:09:38 +0800

    deploy test

--- environments/prod/common/config/main-local.php
+++ environments/prod/common/config/main-local.php
@@ -5,7 +5,7 @@ return [
             'class' => 'yii\db\Connection',
             'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
             'username' => 'root',
-            'password' => '12345',
+            'password' => 'root',
             'charset' => 'utf8',
         ],
         'mailer' => [
