0ea58dea27692f0f495ffaa6f8e0737ab0239a46
孙宗衡 <sunzh@huntor.cn>
2017-05-24 19:37:47 +0800

    deploy ready

---
+++ backend/config/main-local.php
@@ -0,0 +1,25 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'k7H0BsdiBvpJceFWD--0zr4SAkZFC8k6',
+        ],
+    ],
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = [
+        'class' => 'yii\debug\Module',
+    ];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = [
+        'class' => 'yii\gii\Module',
+    ];
+}
+
+return $config;
---
+++ backend/config/params-local.php
@@ -0,0 +1,4 @@
+<?php
+return [
+  'domain' => 'http://localhost:8001/',
+];
