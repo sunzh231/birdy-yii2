249e057168dfbee4c41a0c5918d10cb3bc852c1e
孙宗衡 <sunzh@huntor.cn>
2017-06-05 08:01:25 +0800

    jssdk share to friend

--- webapp/ui/src/components/LoanApply.vue
+++ webapp/ui/src/components/LoanApply.vue
@@ -121,12 +121,11 @@ export default {
   },
   created () {
     this.pageInit()
-    console.log(location.href.split('#')[0])
     let url = location.href.split('#')[0]
     this.birdyService.get(`/api/wechat/config?url=${url}`).then((resp) => {
       this.config = resp
       this.$wechat.config({
-        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: resp.appId, // 必填，公众号的唯一标识
         timestamp: resp.timestamp, // 必填，生成签名的时间戳
         nonceStr: resp.nonceStr, // 必填，生成签名的随机串
@@ -136,9 +135,9 @@ export default {
 
       this.$wechat.onMenuShareAppMessage({
         title: '微信贷款在线申请', // 分享标题
-        desc: '', // 分享描述
+        desc: '微信官方贷款在线申请', // 分享描述
         link: 'http://loan.bailongsheng.cn/webapp', // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
-        imgUrl: '/webappStatic/pic300.png', // 分享图标
+        imgUrl: 'ttp://loan.bailongsheng.cn/upload/base/loan_logo.png', // 分享图标
         type: '', // 分享类型,music、video或link，不填默认为link
         dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
         success: function () {
