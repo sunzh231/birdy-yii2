8ee77e61e533e78326ccb890e773600a15c5b844
孙宗衡 <sunzh@huntor.cn>
2017-06-17 19:10:30 +0800

    refine

--- webapp/ui/src/components/LoanConfirm.vue
+++ webapp/ui/src/components/LoanConfirm.vue
@@ -91,7 +91,7 @@ export default {
     this.birdyService.get(`/api/wechat/config?url=${url}`).then((resp) => {
       this.config = resp
       this.$wechat.config({
-        debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+        debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
         appId: resp.appId, // 必填，公众号的唯一标识
         timestamp: resp.timestamp, // 必填，生成签名的时间戳
         nonceStr: resp.nonceStr, // 必填，生成签名的随机串
